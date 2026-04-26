2a17698e466d4b5f5a68f3bba64844b15466a1ef
Sofiia-D24 <Sofiiadiatlova@gmail.com>
2026-04-26 18:49:09 +0200

    add image search app

---
+++ src/js/render-functions.js
@@ -0,0 +1,52 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        </a>
+        <div class="info">
+          <p><span>${likes}</span><span>Likes</span></p>
+          <p><span>${views}</span><span>Views</span></p>
+          <p><span>${comments}</span><span>Comments</span></p>
+          <p><span>${downloads}</span><span>Downloads</span></p>
+        </div>
+      </li>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('is-visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('is-visible');
+}
